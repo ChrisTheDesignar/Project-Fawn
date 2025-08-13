bccc7f3edac9efa96a595a05e7bce2a51bb8a161
ChrisTheDesignar <trunek.1@gmail.com>
2025-08-13 12:33:38 -0400

    deploy

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_T3HXB from "./testing.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx";
+export default new Map([["./testing.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx", __ASTRO_IMAGE_IMPORT_T3HXB]]);
+		
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  type: 'content',
+
+  // Make the URL /post/<file-name>/ instead of /post/folder/file/
+  slug: ({ id }) => id.split('/').pop()!.replace(/\.(md|mdx)$/, ''),
+
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      pubDate: z.coerce.date(),
+      draft: z.boolean().default(false),
+      // cover image lives next to the .mdx file
+      cover: image().optional(),
+      coverAlt: z.string().optional(),
+    }),
+});
+
+export const collections = { posts };
