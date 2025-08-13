4eaa79161519374ca3dfacfdc77549843bbf24bd
ChrisTheDesignar <trunek.1@gmail.com>
2025-08-13 16:29:58 -0400

    deploy: content + styles

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_T3HXB from "./testing.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx";
-export default new Map([["./testing.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx", __ASTRO_IMAGE_IMPORT_T3HXB]]);
+import __ASTRO_IMAGE_IMPORT_23QMn3 from "./3d-printed-ski-boot-sketch.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwhat-is-this%2Fwhat-is-this.mdx";
+export default new Map([["./testing.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx", __ASTRO_IMAGE_IMPORT_T3HXB], ["./3d-printed-ski-boot-sketch.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fwhat-is-this%2Fwhat-is-this.mdx", __ASTRO_IMAGE_IMPORT_23QMn3]]);
 		
