a3d3fec87194b554478a6fa9524fac7444cfd357
ChrisTheDesignar <trunek.1@gmail.com>
2025-09-24 17:45:49 -0400

    feat: add Giscus comments and fix Formspree endpoint; layout polish

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/posts/testing/testing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/powder-innovation/powder-innovation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpowder-innovation%2Fpowder-innovation.mdx&astroContentModuleFlag=true")],
+["src/content/posts/testing/testing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/printed-footbeds-and-boot-parts/printed-footbeds-and-boot-parts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fprinted-footbeds-and-boot-parts%2Fprinted-footbeds-and-boot-parts.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/what-is-this/what-is-this.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fwhat-is-this%2Fwhat-is-this.mdx&astroContentModuleFlag=true")]]);
 		
