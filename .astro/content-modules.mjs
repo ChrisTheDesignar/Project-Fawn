84ad379d0abe675c0c1348aef656904ccdab947b
ChrisTheDesignar <trunek.1@gmail.com>
2025-10-22 16:06:34 -0400

    research card edits. list gone through, some entries missing? will import from git history next sesh

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/posts/meta-boot/meta-boot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmeta-boot%2Fmeta-boot.mdx&astroContentModuleFlag=true")],
-["src/content/posts/powder-innovation/powder-innovation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpowder-innovation%2Fpowder-innovation.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/research-2026/research-2026.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fresearch-2026%2Fresearch-2026.mdx&astroContentModuleFlag=true")],
+["src/content/posts/powder-innovation/powder-innovation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpowder-innovation%2Fpowder-innovation.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/testing/testing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftesting%2Ftesting.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/printed-footbeds-and-boot-parts/printed-footbeds-and-boot-parts.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fprinted-footbeds-and-boot-parts%2Fprinted-footbeds-and-boot-parts.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/what-is-this/what-is-this.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fwhat-is-this%2Fwhat-is-this.mdx&astroContentModuleFlag=true")]]);
